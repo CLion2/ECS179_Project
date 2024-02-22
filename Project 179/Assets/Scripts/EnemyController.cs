c5883bae5a189df5386da28d5ba595351d60fc56
david west <dt89eor@gmail.com>
2024-02-21 23:31:20 -0800

    added enemy controller, prisoner and gladator files

---
+++ Project 179/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController
+{
+    void Movement()
+    {
+        
+    }
+    void Attack()
+    {
+
+    }
+    void CheckDead()
+    {
+
+    }
+    void TakeDamage()
+    {
+
+    }
+    void Block()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+}
---
+++ Project 179/Assets/Scripts/Gladiator.cs
@@ -0,0 +1,15 @@
+public class Gladiator
+{
+    public float Health { get; }
+    public float AttackDmg { get; }
+    public float AttackSpd { get; }
+    public float RageMeter { get; }
+
+    public Gladiator()
+    {
+        this.Health = 150; // make this at least 1x higher than prisoner
+        this.AttackDmg = 15; // make damage higher
+        this.AttackSpd = 1.0;
+        this.RageMeter = 0; // rage meter should increase as time or damage happens
+    }
+}
