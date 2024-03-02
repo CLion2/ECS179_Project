e097f9be37a64b537c0d9923e686ead2b4f7d81b
david west <dt89eor@gmail.com>
2024-03-02 01:00:52 -0800

    got it working for prisoner only rn

--- Project 179/Assets/Scripts/EnemyController.cs
+++ Project 179/Assets/Scripts/EnemyController.cs
@@ -10,12 +10,12 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private GameObject EnemyPrefab;
     [SerializeField] private GameObject EnemySpawnPrisoner;
     [SerializeField] private GameObject EnemySpawnGladiator;
-    private bool TutorialDone = false;
-    void Build()// change this to build
+    [SerializeField] private bool TutorialDone = false;
+    void Awake()// change this to build
     { // should work the second its created
         GameObject Enemy = Instantiate(EnemyPrefab,EnemySpawnPrisoner.transform.position,Quaternion.identity);
 
-        Destroy(Enemy, 1f);// in case an issue occurs it auto deletes the enemy
+        Destroy(Enemy, 15f);// in case an issue occurs it auto deletes the enemy
     }
 
     // checks if the stage itself is done to switch to start spawn on the gladiator
