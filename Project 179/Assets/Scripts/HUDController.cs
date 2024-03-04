c5efa20cc56dbc86eb9bb48a4e077b4290a9c28a
adbayani <adbayani@ucdavis.edu>
2024-03-04 00:30:52 -0800

    fixed stuff

--- Project 179/Assets/Scripts/HUDController.cs
+++ Project 179/Assets/Scripts/HUDController.cs
@@ -1,18 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class HUDController : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    private HealthBar playerHealth;
+    [SerializeField]
+    private GameObject playerStamina;
+    [SerializeField]
+    private GameObject playerToolTip;
+    [SerializeField]
+    private HealthBar enemyHealth;
+    [SerializeField]
+    private GameObject enemyTitle;
+    private float maxHealth = 100f;
+    private float currentHealth;
+    private bool HPactive = true;
+    private bool enemyActive = false;
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        playerHealth.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentHealth -= 20;
+            playerHealth.SetHealth(currentHealth);
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            HPactive = !HPactive;
+            playerHealth.gameObject.SetActive(HPactive);
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            enemyActive = !enemyActive;
+            playerHealth.gameObject.SetActive(enemyActive);
+        }
     }
 }
