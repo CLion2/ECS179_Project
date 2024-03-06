14c3f4b8b0d87548df89c9ebf8b1bfd22d672e2c
Soma Matsumoto <soma_matsumoto@Somas-MacBook-Pro.local>
2024-03-05 23:42:14 -0800

    Polish the lock-on feature

--- Project 179/Assets/Scripts/MouseLook.cs
+++ Project 179/Assets/Scripts/MouseLook.cs
@@ -70,6 +70,7 @@ public class MouseLook : MonoBehaviour
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
 
+        transform.rotation = lastRotation;
     }
 
     // Lock the camera @ the target
