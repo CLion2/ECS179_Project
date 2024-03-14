b85ce1534595b297d4501bacb2a190ea3c7d6ce4
david west <dt89eor@gmail.com>
2024-03-14 14:15:15 -0700

    added sandbag.cs

--- Project 179/Assets/Scripts/Prisoner.cs
+++ Project 179/Assets/Scripts/Prisoner.cs
@@ -1,15 +1,15 @@
 public class Prisoner
 {
-    public float Health { get; set; }
-    public float AttackDmg { get; set; }
-    public float AttackSpd { get; set; }
-    public float RageMeter { get; set; }
+    public float Health { get; }
+    public float AttackDmg { get; }
+    public float AttackSpd { get; }
+    public float RageMeter { get; }
 
     public Prisoner()
     {
-        this.Health = 150f;
-        this.AttackDmg = 15f;
+        this.Health = 150;
+        this.AttackDmg = 15;
         this.AttackSpd = 1.0f;
-        this.RageMeter = 0f; // should never increase for prisoner just there to show there is a rage meter
+        this.RageMeter = 0; // should never increase for prisoner just there to show there is a rage meter
     }
 }
